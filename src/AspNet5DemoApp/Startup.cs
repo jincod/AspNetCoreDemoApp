2f401384a8c99b3f7fb0e53bd1e4b974a9eaee5c
jincod <vadim.abdrashitov@gmail.com>
2016-10-20 15:01:11 +0500

    use .net core

--- src/AspNet5DemoApp/Startup.cs
+++ src/AspNet5DemoApp/Startup.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AspNet5DemoApp
