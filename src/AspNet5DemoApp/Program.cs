a27cbc765c24661e846d7d82b74aab9d3318f2b2
jincod <vadim.abdrashitov@gmail.com>
2016-10-21 12:12:12 +0500

    parse cli args

--- src/AspNet5DemoApp/Program.cs
+++ src/AspNet5DemoApp/Program.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace AspNet5DemoApp
 {
@@ -7,9 +8,12 @@ namespace AspNet5DemoApp
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
                 .UseStartup<Startup>()
                 .Build();
 
