cef3a7211ba27ac61f66c82d14d18c19c27f063a
jincod <vadim.abdrashitov@gmail.com>
2018-06-01 11:27:31 +0500

    Updated to .NET Core 2.1

--- src/AspNetCoreDemoApp/Program.cs
+++ src/AspNetCoreDemoApp/Program.cs
@@ -5,16 +5,11 @@ namespace AspNetCoreDemoApp
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+        public static void Main(string[] args) =>
+            CreateWebHostBuilder(args).Build().Run();
 
-        public static IWebHost BuildWebHost(string[] args)
-        {
-            return WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-        }
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
