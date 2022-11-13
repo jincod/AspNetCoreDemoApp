28c896e427ceb419b3d6590f47e141375c23fb83
jincod <vadim.abdrashitov@gmail.com>
2022-11-13 17:40:50 +0500

    updated packages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './client/app';
 import './client/style.css';
 
-ReactDOM.render(
-	<App />,
-	document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
